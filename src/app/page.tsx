717873b4a1abe4ed0ce3e78e6b59e6cea23fea42
isha <IshaGajendrasingh.Thakur@student.ufv.ca>
2024-11-10 14:49:59 -0800

    added shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
       <main>
         <h1>Welcome to pizza shop</h1>
+        <Button variant="outline">Button</Button>
+
       </main>
-      
+
   );
 }
